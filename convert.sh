cd320ba91507f53f649f13b8e454428c0297ff03
JonHub <jon.driscoll@gmail.com>
2021-06-06 16:58:41 -0700

    Spelling and typos in README

---
+++ convert.sh
@@ -0,0 +1,2 @@
+# converts the notebook to single file .html
+time jupyter nbconvert --to html --execute notebooks/stacasso_guide.ipynb
--- src/convert.sh
+++
@@ -1,2 +0,0 @@
-# converts the notebook to single file .html
-jupyter nbconvert stacasso_introduction.ipynb --to html
